f318689d5af4f19e8bae937362ddf4386a4c239a
James E. Marca <james@activimetrics.com>
2014-11-12 10:12:14 -0800

    refactored a bit, fixed up Makefile
    refactoring wasn't necessary.  Hunting a bug down the wrong rabbit
    hole.  Anyway, I found a minor bug by doing it (incorrect reference to a
    variable out of scope)
    
    big edits to Makefile.  Now in the future you can add tests by creating
    a file "blahblah.md" and then the corresponding "blahblah.js" that tests
    what you expect from the markdown.  The Makefile will spit out the tex,
    then run the .js file, then clean the tex
    
    as before can be run with npm test or make; make test; make clean

--- Makefile
+++ Makefile
@@ -1,20 +1,59 @@
-# A Makefile to generate a ConTeXt file, paper.tex from paper.md
-#paper.tex: paper.md
-PANDOC  :=  pandoc -t context --filter=context-float-refs.js
 #
-#test:
-#    mocha test/test-filter.js
+# 'make depend' uses makedepend to automatically generate dependencies
+#               (dependencies are added to end of Makefile)
+# 'make'        build executable file 'mycc'
+# 'make clean'  removes all .tex and executable files
 #
-#clean:
-#    rm test/paper.tex
-#
-# An example Makefile to compile a context file, paper.tex
 
-paper.tex: test/paper.md
-	$(PANDOC) test/paper.md   -o test/paper.tex
+# define the C compiler to use
+##PANDOC  :=  pandoc -t context --filter=context-float-refs.js
+PANDOC = pandoc
+
+# define any compile-time flags
+PANDOCFLAGS = -t context -f markdown --filter=context-float-refs.js
+
+# define the C source files
+SRCS = test/paper.md ## test/partial.md test/traveltime.md test/traveltime_noR.md
+
+# define the C object files
+#
+# This uses Suffix Replacement within a macro:
+#   $(name:string1=string2)
+#         For each word in 'name' replace 'string1' with 'string2'
+# Below we are replacing the suffix .md of all words in the macro SRCS
+# with the .tex suffix
+#
+OBJS  = $(SRCS:.md=.tex)
+TESTS = $(SRCS:.md=.js)
+
+REPORTER = dot
+TIMEOUT = 5000
+
+pretest:    $(OBJS)
+	@echo  test cases $(OBJS) created
+
+# this is a suffix replacement rule for building .tex's from .md's
+# it uses automatic variables $<: the name of the prerequisite of
+# the rule(a .md file) and $@: the name of the target of the rule (a .tex file)
+# (see the gnu make manual section about automatic variables)
+%.tex: %.md
+	$(PANDOC) $(PANDOCFLAGS)  $<  -o $@
+
 
 test:
-	mocha test/test.js
+	@NODE_ENV=test  ./node_modules/.bin/mocha \
+	--reporter $(REPORTER) \
+	--timeout 5000 \
+	--growl \
+	$(TESTS)
 
 clean:
-	rm test/paper.tex
+	$(RM) $(OBJS) *~
+
+#
+# The following part of the makefile is generic; it can be used to
+# build any executable just by changing the definitions above and by
+# deleting dependencies appended to the file from 'make depend'
+#
+
+.PHONY: pretest test clean
--- context-float-refs.js
+++ context-float-refs.js
@@ -7,10 +7,10 @@ var _ = require('lodash')
 var Str = pandoc.Str;
 
 function collapse(arr) {
-  return _.reduce(arr, function(s,v) { 
+  return _.reduce(arr, function(s,v) {
     if ( v.t === "Space" ) return s + " "
     else if ( v.t === "Str" ) return s + v.c
-    else throw new Error("Don't know how to handle "+JSON.stringify(v)) 
+    else throw new Error("Don't know how to handle "+JSON.stringify(v))
   },"")
 }
 
@@ -25,68 +25,13 @@ function emit(s) {
 function labelFromCaption(c) {
 }
 
-function action(type,value,format,meta) {
-/*
-  emit("type:"+JSON.stringify(type,null,'  ')+"\n")
-  emit("type:"+JSON.stringify(value,null,'  ')+"\n")
-  emit(JSON.stringify(format,null,'  ')+"\n")
-  emit(JSON.stringify(meta,null,'  ')+"\n")
-*/
-//  if (type === 'Str') return Str(value.toUpperCase());
-  if (type === 'Para'
-      && value.length > 0 && value[0].t === 'Image' ) {
-    var caption = collapse(value[0].c[0])
-    var target  = value[0].c[1][0]
-    var meta    = collapse(value.slice(1))
-    var place   = "here"
-    var label   = ""
-    var m;
-    emit(["caption:"+caption,
-          "target:"+target,
-          "meta:"+meta,
-          ""].join("\n"))
-    var figmeta = ""
-    if ( m = caption.match(/^(.*)\s*\{([^}]*)\}\s*(.*)$/) ) {
-      figmeta = m[2]
-      caption = m[1]+m[3]
-    } else if ( m = meta.match(/{(.*)(#)(fig:[^\s}]*)(.*)\}/) ) {
-      figmeta = meta
-    } 
-
-    var classes = []
-    _.each(figmeta.split(/\s+/), function(it) {
-      if ( m = it.match(/#(fig:.*)/) ) {
-        label = m[1]
-      } else if ( m=it.match(/place=(.*)/) ) {
-        place = m[1]
-      } else {
-        classes.push(it)
-      }
-    })
-
-    if ( label === "" ) {
-      // try setting the label by the stripped file name
-      if ( m = target.match(/([^\/]*\/(.*)\.(.*))/) ) {
-        label = "fig:"+m[2]
-      }
-    }
-    return {t:'RawBlock',
-            c:["context",
-               ["\\placefigure",
-                "[",
-                place,
-                "][",
-                label,
-                "]{",
-                caption,
-                "}{\\externalfigure[",
-                target,
-                "]}"].join("")]}
-  } else if ( type === 'Table' ) {
+function handleTable(value){
     var caption = collapse(value[0]);
-    var label = ""
+    // var target  = value[0].c[1][0]
     var meta = ""
     var place = "here"
+    var label = ""
+    var m;
 
     var tabmeta = ""
     if ( m = caption.match(/^(.*)\s*\{([^}]*)\}\s*(.*)$/) ) {
@@ -94,7 +39,7 @@ function action(type,value,format,meta) {
       caption = m[1]+m[3]
     } else if ( m = meta.match(/{(.*)(#)(fig:[^\s}]*)(.*)\}/) ) {
       tabmeta = meta
-    } 
+    }
 
     var classes = []
     _.each(tabmeta.split(/\s+/), function(it) {
@@ -127,8 +72,8 @@ function action(type,value,format,meta) {
                "|]"].join("")
     txt += "\\HL"+
       [_.map(heads,function(h) {
-/*        return "\\NC "+_.map(h.c,function(hh) { 
-          return collapse(hh.c) 
+/*        return "\\NC "+_.map(h.c,function(hh) {
+          return collapse(hh.c)
         }).join("")
 */
         return "\\NC "+_.map(h,function(hh) { return collapse(hh.c) }).join("")
@@ -141,7 +86,71 @@ function action(type,value,format,meta) {
     txt += "\\HL\n\\stoptable"
     return { t:'RawBlock',
              c:["context", txt] }
-    
+}
+
+function handleFigure(value){
+    var caption = collapse(value[0].c[0])
+    var target  = value[0].c[1][0]
+    var meta    = collapse(value.slice(1))
+    var place   = "here"
+    var label   = ""
+    var m;
+    emit(["caption:"+caption,
+          "target:"+target,
+          "meta:"+meta,
+          ""].join("\n"))
+    var figmeta = ""
+    if ( m = caption.match(/^(.*)\s*\{([^}]*)\}\s*(.*)$/) ) {
+      figmeta = m[2]
+      caption = m[1]+m[3]
+    } else if ( m = meta.match(/{(.*)(#)(fig:[^\s}]*)(.*)\}/) ) {
+      figmeta = meta
+    }
+
+    var classes = []
+    _.each(figmeta.split(/\s+/), function(it) {
+      if ( m = it.match(/#(fig:.*)/) ) {
+        label = m[1]
+      } else if ( m=it.match(/place=(.*)/) ) {
+        place = m[1]
+      } else {
+        classes.push(it)
+      }
+    })
+
+    if ( label === "" ) {
+      // try setting the label by the stripped file name
+      if ( m = target.match(/([^\/]*\/(.*)\.(.*))/) ) {
+        label = "fig:"+m[2]
+      }
+    }
+    return {t:'RawBlock',
+            c:["context",
+               ["\\placefigure",
+                "[",
+                place,
+                "][",
+                label,
+                "]{",
+                caption,
+                "}{\\externalfigure[",
+                target,
+                "]}"].join("")]}
+}
+
+function action(type,value,format,meta) {
+/*
+  emit("type:"+JSON.stringify(type,null,'  ')+"\n")
+  emit("type:"+JSON.stringify(value,null,'  ')+"\n")
+  emit(JSON.stringify(format,null,'  ')+"\n")
+  emit(JSON.stringify(meta,null,'  ')+"\n")
+*/
+
+  if (type === 'Para'
+      && value.length > 0 && value[0].t === 'Image' ) {
+      return handleFigure(value)
+  } else if ( type === 'Table' ) {
+      return handleTable(value)
   } else
     return null
 
@@ -150,4 +159,3 @@ function action(type,value,format,meta) {
 pandoc.stdio(action);
 //var json = JSON.parse(fs.readFileSync('/dev/stdin').toString())
 //emit(JSON.stringify(json,null,'  '))
-
